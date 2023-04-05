b60e95003fac34c570b8744ff49d61ea0cd14b47
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-04-05 17:15:58 -0400

    created display_welcome function

--- game.py
+++ game.py
@@ -6,3 +6,9 @@ class Game():
         self.human_player2 = Human('Player 2')
         self.ai1 = AI('Computer 1')
         self.ai2 = AI('Computer 2')
+
+    def run_game():
+        pass
+
+    def display_welcome():
+        print("Welcome to Rock Paper Scissors Lizard Spock. \n\n Each match will be best out of three games. \n Use the number keys to enter your selction\n\n Scissors cut Paper \n Paper covers Rock \n Rock crushes Lizard \n Lizard poisons Spock \n Scissors decapitates Lizard \n Lizard eats Paper \n Paper disproves Spock \n Spock vaporizes Rock \n Rock crushes Scissors")
