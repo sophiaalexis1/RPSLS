5bd4aaa91f119f98d630711f3b1e1379ac7b9af5
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-04-05 17:36:16 -0400

    created number of players function in game class

--- game.py
+++ game.py
@@ -7,8 +7,16 @@ class Game():
         self.ai1 = AI('Computer 1')
         self.ai2 = AI('Computer 2')
 
-    def run_game():
+    def run_game(self):
         pass
 
-    def display_welcome():
+    def display_welcome(self):
         print("Welcome to Rock Paper Scissors Lizard Spock. \n\n Each match will be best out of three games. \n Use the number keys to enter your selction\n\n Scissors cut Paper \n Paper covers Rock \n Rock crushes Lizard \n Lizard poisons Spock \n Scissors decapitates Lizard \n Lizard eats Paper \n Paper disproves Spock \n Spock vaporizes Rock \n Rock crushes Scissors")
+    
+    def number_players(self):
+        self.number_of_players = input("How many players? 1 or 2")
+        if self.number_of_players == "1":
+            run_game(human_ai)
+        elif self.number_of_players == "2":
+            run_game(humann_human)
+
